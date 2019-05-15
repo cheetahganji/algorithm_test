4eb13324c52b06b6d4236c94938fa300cb708470
cheetahganji <igloo2@naver.com>
2019-05-15 18:56:44 +0900

    Update baby_shark_16236(int).py

--- baby_shark_16236(int).py
+++ baby_shark_16236(int).py
@@ -1,144 +1,195 @@
 '''
-bs = [pos:(), size:2, move:0]
-
-
+1. 아기상어의 현재 위치에서 먹을 수 있는 가장 가까운 물고기를 찾는다.
+2. 만약 먹을 수 있는 물고기가 없으면 종료
+3. 아니라면, 해당 물고기 위치로 이동한다.
+4. 아기상어가 커질 수 있으면 커진다.
+5. 1번부터 반복한다
 '''
 
-class Node:
-    def __init__(self, pos, lv):
-        self.pos = pos
-        self.lv = lv
-        self.next = None
-
-    
-
-
-class Queue:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def is_empty(self):
-        if not self.head:
-            return True
-        else:
-            return False
-
-    def enqueue(self, node):
-        if self.is_empty():
-            self.head = node
-            self.tail = node
-        else:
-            self.tail.next = node
-            self.tail = node
-
-    def dequeue(self):
-        if self.is_empty():
-            return None
-
-        ret_node = self.head
-        self.head = self.head.next
-        return ret_node
-
-    def peek(self):
-        if self.is_empty():
-            return None
-
-        return self.head
-
-
+import queue
+import time
 
 class BabyShark:
-    def __init__(self, board):
-        self.pos = [0, 0]
-        self.move_cnt = 0
-        self.size = 2
+    def __init__(self, board, shark_no, shark_size):
         self.board = board
-        self.set_bs_stat()
+        self.shark_size = shark_size
+        self.shark_no = shark_no
+        self.fish_cnt, self.fish_cnt_tot, self.move_cnt = 0, 0, 0
 
-    def set_bs_stat(self):
-        for i, v in enumerate(self.board):
-            if 9 in v:
-                self.pos =[i, v.index(9)]
+        self.set_shark_pos()
 
-    def next_fish_pos(self):
+    def print_info(self):
+        print('self.board :')
+        for i in self.board:
+            print(i)
+
+    #     print('self.shark_size :', self.shark_size)
+    #     print('self.fish_cnt :', self.fish_cnt)
+    #     print('self.fish_cnt_tot :', self.fish_cnt_tot)
+    #     print('self.move_cnt :', self.move_cnt)
+    #     print('')
+        
+
+    def set_shark_pos(self):
         board_len = len(self.board)
-        eatable_fish_pos = []
-
         for r in range(board_len):
             for c in range(board_len):
-                if self.board[r][c] < self.size and self.board[r][c] in range(1,7):
-                    eatable_fish_pos.append([r,c])
-        
-        print('eatable_fish_pos :', eatable_fish_pos)
-
-        num_of_pos = len(eatable_fish_pos)
-        if num_of_pos == 0:
-            return None
-        elif num_of_pos == 1:
-            return eatable_fish_pos[0]
-        else:
-            return self.get_shortest_pos(eatable_fish_pos)
+                if board[r][c] == self.shark_no:
+                    self.shark_pos = [r, c]
+                    break
 
 
-    def get_shortest_pos(self, eatable_fish_pos):
-        tmp_shark_pos = self.pos
-        # #temp
-        # 1: 한 번 움직였을 때 
-        # 2: 1에서 한번 움직여서 갈 수 있는 위치
-        # dp[n] : n번 움직여서 갈 수 있는 위치
-        # dp[n] : dp[n-1] + ( [-1,0], [1,0], [0,-1],[0,1] )
+    # 아기상어 크기보다 작은 물고기가 있는지
+    def is_exists_eatable_fish(self):
+        is_exists = False
 
-        return [-1, -1]
+        for r in self.board:
+            for c in r:
+                if self.shark_size > c:
+                    is_exists = True
+                    break
+
+        return is_exists
 
 
-    def get_next_pos(self, pos):
-        print('pos :', pos)
+    def get_move_one(self, pos):
         r, c= pos[0], pos[1]
         board_len = len(self.board)
-
         m = list()
-        if r-1 >= 0 and board[r-1][c] <= self.size:
+
+        if r-1 >= 0 and board[r-1][c] <= self.shark_size and [r-1, c] != self.shark_pos:
             m.append([r-1, c])
         
-        if r+1 <= board_len-1 and board[r+1][c] <= self.size:
+        if r+1 <= board_len-1 and board[r+1][c] <= self.shark_size and [r+1, c] != self.shark_pos:
             m.append([r+1, c])
 
-        if c-1 >= 0 and board[r][c-1] <= self.size:
+        if c-1 >= 0 and board[r][c-1] <= self.shark_size and [r, c-1] != self.shark_pos:
             m.append([r, c-1])
 
-        if c+1 <= board_len-1 and board[r][c+1] <= self.size:
+        if c+1 <= board_len-1 and board[r][c+1] <= self.shark_size and [r, c+1] != self.shark_pos:
             m.append([r, c+1])
 
         return m
 
 
+    def get_pick_one(self, pos):
+        if not pos:
+            return pos
+        else:
+            pos.sort()
+            return pos[0]
+
+
+    def get_next_fish_info(self):
+        # print('\n==========================================')
+        next_fish_pos, move = list(), 0
+
+        # if self.is_exists_eatable_fish():
+        std_pos = [self.shark_pos, 0]
+
+        visited_pos = list()
+        visited_pos.append(self.shark_pos)
+
+        q = queue.Queue()
+        q.put_nowait(std_pos)
+
+        same_move = 0
+
+        while(q.qsize()>0):
+            
+            p = q.get_nowait()
+            l = p[1]
+
+            # print('next_fish_pos : ', next_fish_pos)
+            # print('l : ', l)
+            # print('same_move : ', same_move)
+
+            if next_fish_pos and l == same_move:
+                break
+
+            try:
+                fish_pos = self.get_move_one(p[0])
+                fish_pos.sort()
+
+                for fp in fish_pos:
+                    # print('fp ', fp)
+                    a, b = fp[0], fp[1]
+                    if board[a][b] < self.shark_size and board[a][b] > 0:
+                        next_fish_pos.append(fp)
+                        move = l+1
+                        same_move = move
+                        # break
+                
+                if not next_fish_pos:   # next_fish_pos가 없으면
+                    
+                    for fp in fish_pos:
+                        if fp not in visited_pos:
+                            # print('fp ', fp)
+                            q.put_nowait([fp, l+1])
+                            visited_pos.append(fp)
+             
+
+            except Exception as e:
+                # print('더 이상 없음 :', e)
+                break
+        # print('next_fish_pos, move', next_fish_pos, move)
+        # print('==========================================\n')
+
+        return self.get_pick_one(next_fish_pos), move
+
+                        
+    def move(self, pos, move):
+        r, c = self.shark_pos[0], self.shark_pos[1]
+        r2, c2 = pos[0], pos[1]
+        self.board[r][c] = 0
+        self.shark_pos = pos
+        self.board[r2][c2] = self.shark_no
+        self.move_cnt += move
+
+        self.fish_cnt += 1
+        self.fish_cnt_tot += 1
+
+        if self.shark_size == self.fish_cnt:
+            self.shark_size += 1
+            self.fish_cnt = 0
+
+        
 
 if __name__ == '__main__':
-    n = 4
+
+    n = 6
     board = [
-        [4, 3, 2, 1],
-        [0, 0, 0, 0],
-        [0, 0, 9, 0],
-        [1, 2, 3, 4]
+[5, 4, 3, 2, 3, 4],
+[4, 3, 2, 3, 4, 5],
+[3, 2, 9, 5, 6, 6],
+[2, 1, 2, 3, 4, 5],
+[3, 2, 1, 6, 5, 4],
+[6, 6, 6, 6, 6, 6]
     ]
 
-    for i in board:
-        print(i)
+    shark_no = 9
+    shark_size = 2
 
+    n = int(input())
+    board = []
+    for i in range(n):
+        board.append(list(map(int, input().split(' '))))
 
-    bs = BabyShark(board)
-    print(bs.next_fish_pos())
-
-    print(bs.get_next_pos([1, 2]))
-
-
-    q = Queue()
+    # start_time = time.time() 
 
     
-    q.enqueue(Node((1,2), 1))
-    q.enqueue(Node((1,3), 1))
+    bs = BabyShark(board, shark_no, shark_size)
+
+    next_fish_pos, move = bs.get_next_fish_info()    
+
+    while(next_fish_pos):
+        bs.move(next_fish_pos, move)
+        # bs.print_info()
+        next_fish_pos, move = bs.get_next_fish_info()
+        
+    print(bs.move_cnt)
 
 
-    print(q.peek())
 
+    # end_time = time.time()
+    # print("WorkingTime: {} sec".format(end_time-start_time))
